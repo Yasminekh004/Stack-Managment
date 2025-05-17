2ef9a828b4a0327404ad26d8a5d276ff703e939a
RayenTB <146642578+RayenTB@users.noreply.github.com>
2025-05-17 22:40:32 +0100

    Update User.java
    Added Budget field

--- src/main/java/com/codingdojo/stackmanagement/models/User.java
+++ src/main/java/com/codingdojo/stackmanagement/models/User.java
@@ -24,28 +24,43 @@ import jakarta.validation.constraints.Size;
 @Table(name = "users")
 public class User {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-	@NotEmpty(message = "First Name is required!")
-	@Size(min = 3, max = 30, message = "Name must be between 3 and 30 characters")
-	private String firstName;
+    @NotEmpty(message = "First Name is required!")
+    @Size(min = 3, max = 30, message = "Name must be between 3 and 30 characters")
+    private String firstName;
 
-	@NotEmpty(message = "Last Name is required!")
-	@Size(min = 3, max = 30, message = "Name must be between 3 and 30 characters")
-	private String lastName;
+    @NotEmpty(message = "Last Name is required!")
+    @Size(min = 3, max = 30, message = "Name must be between 3 and 30 characters")
+    private String lastName;
 
-	@NotEmpty(message = "Email is required!")
-	@Email(message = "Please enter a valid email!")
-	private String email;
+    @NotEmpty(message = "Email is required!")
+    @Email(message = "Please enter a valid email!")
+    private String email;
 
-	@NotEmpty(message = "Password is required!")
-	@Size(min = 8, max = 128, message = "Password must be between 8 and 128 characters")
-	private String password;
+    @NotEmpty(message = "Password is required!")
+    @Size(min = 8, max = 128, message = "Password must be between 8 and 128 characters")
+    private String password;
 
-	@Transient // this tag is used to avoid saving the confirm pwd to the db
-	@NotEmpty(message = "Confirm Password is required!")
-	@Size(min = 8, max = 128, message = "Confirm Password must be between 8 and 128 characters")
-	private String confirm;
+    @Transient // This avoids saving confirm to the DB
+    @NotEmpty(message = "Confirm Password is required!")
+    @Size(min = 8, max = 128, message = "Confirm Password must be between 8 and 128 characters")
+    private String confirm;
+
+    //Budget field
+    @Column(nullable = false)
+    private Double budget = 0.0;
+
+    //GETTER & SETTER for budget
+    public Double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(Double budget) {
+        this.budget = budget;
+    }
+
+    // for later GETTER & SETTER
 }
