8237e25970d1c981d95109f75364ee569fa0f896
RayenTB <146642578+RayenTB@users.noreply.github.com>
2025-05-18 13:49:56 +0100

    Update UserRepository.java

--- src/main/java/com/codingdojo/stackmanagement/repositories/UserRepository.java
+++ src/main/java/com/codingdojo/stackmanagement/repositories/UserRepository.java
@@ -1,5 +1,16 @@
 package com.codingdojo.stackmanagement.repositories;
 
-public interface UserRepository {
+import java.util.List;
+import java.util.Optional;
 
-}
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codingdojo.stackmanagement.models.User;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+	List<User> findAll();
+	Optional<User> findByEmail(String email);
+ 
+}//end repository 
